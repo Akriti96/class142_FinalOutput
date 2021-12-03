308d732386f1c21be0d8d822eb00fa568ed9e42f
Akriti96 <78339246+Akriti96@users.noreply.github.com>
2021-12-03 23:19:26 +0530

    Add files via upload

---
+++ demgraphic.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+df= pd.read_csv("final.csv")
+
+C= df["vote_average"].mean()
+m=df["vote_count"].quantile(0.9)
+
+qmovies=df.copy().loc[df["vote_count"]>=m]
+def weightRating(x,m=m,C=C):
+  v=x["vote_count"]
+  R= x["vote_average"]
+  return (v/(v+m)*R)+(m/(v+m)*C)
+qmovies["score"]=qmovies.apply(weightRating,axis=1)
+qmovies=qmovies.sort_values("score",ascending=False)
+
+output=qmovies[["title_x","movieposter","release_date","runtime","overview","vote_average"]].values.tolist()
+# print(output)
